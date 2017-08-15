7c1283c84a1570e37e61980824bf39ebbb937276
Vinayak <karkhanisvinayak@outlook.com>
2017-08-15 20:49:58 +0530

    Android customized calander

---
+++ app/src/main/java/com/example/vinayak/androidcalander/CustomCalendarActivity.java
@@ -0,0 +1,14 @@
+package com.example.vinayak.androidcalander;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomCalendarActivity extends AppCompatActivity {
+    private static final String TAG = CustomCalendarActivity.class.getSimpleName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_calendar);
+        CalendarCustomView mView = (CalendarCustomView)findViewById(R.id.custom_calendar);
+    }
+}
---
+++ app/src/main/java/com/example/vinayak/androidcalander/EventObjects.java
@@ -0,0 +1,30 @@
+package com.example.vinayak.androidcalander;
+
+import java.util.Date;
+/**
+ * Created by vinayak on 14/8/17.
+ */
+
+public class EventObjects {
+    private int id;
+    private String message;
+    private Date date;
+    public EventObjects(String message, Date date) {
+        this.message = message;
+        this.date = date;
+    }
+    public EventObjects(int id, String message, Date date) {
+        this.date = date;
+        this.message = message;
+        this.id = id;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public Date getDate() {
+        return date;
+    }
+}
